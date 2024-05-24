302ddbc51594b8b3df27143e15c11fc41e0e5344
JMSBRN <zakhavai@gmail.com>
2024-05-24 15:54:13 +0300

    feat: created tests for Stack

--- src/components/stack/Stack.stories.tsx
+++ src/components/stack/Stack.stories.tsx
@@ -1,33 +1,56 @@
-import Stack, {StackProps} from './Stack';
-import {StoryFn, Meta} from '@storybook/react';
+import Stack, { StackProps } from "./Stack";
+import { StoryFn, Meta } from "@storybook/react";
+import { within, expect } from "@storybook/test";
 
 export default {
-    title: 'Components/Stack',
-    component: Stack,
-    argTypes: {
-        numberOfChildren: {type: 'number', defaultValue: 5},
-    }
+  title: "Components/Stack",
+  component: Stack,
+  argTypes: {
+    numberOfChildren: { type: "number", defaultValue: 5 },
+  },
 } as Meta<typeof Stack>;
 
-const Template: StoryFn<StackProps & {numberOfChildren: number}> = ({numberOfChildren, ...args}) => 
-
-
-<Stack {...args} >
-    {[...Array(numberOfChildren).keys()].map((_, index) => <div>Children{index + 1}</div>)}
-</Stack>;
+const Template: StoryFn<StackProps & { numberOfChildren: number }> = ({
+  numberOfChildren,
+  ...args
+}) => (
+  <Stack {...args}>
+    {[...Array(numberOfChildren).keys()].map((_, index) => (
+      <div>Children{index + 1}</div>
+    ))}
+  </Stack>
+);
+type StackArgs = Partial<StackProps & { numberOfChildren: number }>;
+const playTest = async ({
+  args,
+  canvasElement,
+}: {
+  args: StackArgs;
+  canvasElement: HTMLElement;
+}) => {
+  const canvas = within(canvasElement);
+  const childrenRegex = /Children\s*\d+/;
+  const children = await canvas.findAllByText(childrenRegex);
+  expect(children).toHaveLength(args.numberOfChildren || 5);
+};
 
 export const Horizontal = Template.bind({});
 Horizontal.args = {
-    numberOfChildren: 5,
-    direction: 'row',
-    spacing: 1,
-    wrap: true,
-}
+  numberOfChildren: 15,
+  direction: "row",
+  spacing: 1,
+  wrap: true,
+};
+Horizontal.play = async ({ canvasElement }: { canvasElement: HTMLElement }) =>
+  playTest({ args: Horizontal.args!, canvasElement });
 
 export const Vertical = Template.bind({});
 Vertical.args = {
-    numberOfChildren: 5,
-    direction: 'column',
-    spacing: 5,
-    wrap: false,
-}
+  numberOfChildren: 5,
+  direction: "column",
+  spacing: 5,
+  wrap: false,
+};
+
+Vertical.play = async ({ canvasElement }: { canvasElement: HTMLElement }) =>
+  playTest({ args: Vertical.args!, canvasElement });
